189ee0088e9f8c2df3c89aa58c0208b4d2e8b832
exg906 <gert.driesen@infrabel.be>
2019-04-15 13:51:49 +0200

    Add repro for DocFX issue concerning custom attributes.

---
+++ src/DummyAttribute.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DocFxCustomAttributeProblem
+{
+    [DataContract(Namespace = "http://www.infrabel.be/A572/Infrastructure")]
+    public class DummyAttribute
+    {
+        /// <summary>
+        /// Gets or sets  the user defined identifier.
+        /// </summary>
+        [DataMember(EmitDefaultValue = false)]
+        public long Attribute { get; set; }
+
+    }
+}
---
+++ src/DummyNoAttribute.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DocFxCustomAttributeProblem
+{
+    public class DummyNoAttribute
+    {
+
+        public string NoAttribute { get; set; }
+        
+    }
+}
